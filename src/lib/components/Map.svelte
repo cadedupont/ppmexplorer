3b8076827f8e7ca9bbba833793b1206b54bec0a5
Cade DuPont <cadedupont45@gmail.com>
2024-10-05 11:08:43 -0500

    Add raster tile source to map, add sample logo

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-  import { MapLibre, GeoJSON, FillLayer, LineLayer, DefaultMarker } from 'svelte-maplibre';
+  import {
+    MapLibre,
+    GeoJSON,
+    FillLayer,
+    LineLayer,
+    RasterTileSource,
+    RasterLayer
+  } from 'svelte-maplibre';
 
   export let geojson: GeoJSON.GeometryCollection;
   export let center: [number, number] | undefined;
@@ -9,21 +16,30 @@
   style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
   center={center ? center : [14.4884, 40.75103]}
   zoom={center ? 18 : 15}
+  minZoom={15}
+  maxZoom={18}
   class="map"
   standardControls
 >
+  <RasterTileSource tiles={['https://palp.art/xyz-tiles/{z}/{x}/{y}.png']} tileSize={256}>
+    <RasterLayer
+      paint={{
+        'raster-opacity': 0.6
+      }}
+    />
+  </RasterTileSource>
   {#each geojson.geometries as geometry}
     {#if geometry.type != 'Point'}
       <GeoJSON data={geometry}>
         <FillLayer
           paint={{
-            'fill-color': '#088',
-            'fill-opacity': 0.2
+            'fill-color': 'blue',
+            'fill-opacity': 0.25
           }}
         />
         <LineLayer
           paint={{
-            'line-color': '#000',
+            'line-color': 'blue',
             'line-width': 1
           }}
         />
