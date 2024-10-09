7e62e41dbd8f2cc1e3e3234774bc01fa8b7b7007
Cade DuPont <cadedupont45@gmail.com>
2024-10-09 08:52:12 -0500

    DIsplay cluster marker if multiple results in 1 room

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -8,6 +8,7 @@
     FillLayer,
     LineLayer,
     DefaultMarker,
+    Marker,
     Popup,
     RasterTileSource,
     RasterLayer
@@ -88,7 +89,7 @@
         <FillLayer
           paint={{
             'fill-color': colorMap[feature.properties.color],
-            'fill-opacity': 0.3 * (showMarker ? feature.properties.ppmItemIDs.length : 1)
+            'fill-opacity': 0.2 * (showMarker ? feature.properties.ppmItemIDs.length : 1)
           }}
         />
         <LineLayer
@@ -98,18 +99,34 @@
           }}
         />
         {#if showMarker}
-          <DefaultMarker lngLat={feature.properties.centroid}>
+          <Marker lngLat={feature.properties.centroid}>
+            {#if feature.properties.ppmItemIDs.length === 1}
+              <div class="rounded-full bg-primary-400 p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="6 2 16 20">
+                  <path
+                    fill="currentColor"
+                    d="M14 11.5A2.5 2.5 0 0 0 16.5 9A2.5 2.5 0 0 0 14 6.5A2.5 2.5 0 0 0 11.5 9a2.5 2.5 0 0 0 2.5 2.5M14 2c3.86 0 7 3.13 7 7c0 5.25-7 13-7 13S7 14.25 7 9a7 7 0 0 1 7-7"
+                  />
+                </svg>
+              </div>
+            {:else}
+              <div class="rounded-full bg-primary-600 p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M14 11.5A2.5 2.5 0 0 0 16.5 9A2.5 2.5 0 0 0 14 6.5A2.5 2.5 0 0 0 11.5 9a2.5 2.5 0 0 0 2.5 2.5M14 2c3.86 0 7 3.13 7 7c0 5.25-7 13-7 13S7 14.25 7 9a7 7 0 0 1 7-7M5 9c0 4.5 5.08 10.66 6 11.81L10 22S3 14.25 3 9c0-3.17 2.11-5.85 5-6.71C6.16 3.94 5 6.33 5 9Z"
+                  />
+                </svg>
+              </div>
+            {/if}
             <Popup offset={[0, -10]}>
-              <div class='mb-2 text-lg font-bold text-primary-600'>
+              <div class="mb-2 text-lg font-bold text-primary-500">
                 {urnToTitle(feature.properties.title)}
               </div>
-              <ul class="list-inside list-disc space-y-1 text-primary-600">
+              <ul class="list-inside list-disc space-y-1 text-tertiary-500">
                 {#each feature.properties.ppmItemIDs as itemID}
                   <li class="text-sm">
-                    <a
-                      href={`/item/${itemID}`}
-                      class="text-tertiary-600 hover:text-tertiary-800"
-                    >
+                    <a href={`/item/${itemID}`} class="text-tertiary-600 hover:text-tertiary-700">
                       Volume {parseInt(itemID.split('_')[1])}, Page {parseInt(itemID.split('_')[3])}
                       (Image {parseInt(itemID.split('_')[5])})
                     </a>
@@ -117,7 +134,7 @@
                 {/each}
               </ul>
             </Popup>
-          </DefaultMarker>
+          </Marker>
         {/if}
       </GeoJSON>
     {/if}
